06dec6fd5ff2f67ed61e8373209b1eec0d444fe0
sabahcsxgawd <sabah72224@gmail.com>
2023-06-09 20:11:02 +0600

    need to do search

--- Offline 1/1905118_trial/trial.cpp
+++ Offline 1/1905118_trial/trial.cpp
@@ -1,52 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // parMove values :-> up : 1, down : 2, right : 3, left : 4
 
-class Board {
+class Board
+{
 
 private:
     vector<vector<int>> board;
     int gridSize, gVal, hVal, fVal, hFlag, parMove;
     Board *parent;
 
-
 public:
-    Board(int gridSize, vector<vector<int>> board, Board *parent, int hFlag, int parMove) {
+    Board(int gridSize, vector<vector<int>> board, Board *parent, int hFlag, int parMove)
+    {
         this->gridSize = gridSize;
         this->board = board;
         this->parent = parent;
         this->hFlag = hFlag;
         this->parMove = parMove;
 
-        if(this->parent == nullptr) {
+        if (this->parent == nullptr)
+        {
             this->gVal = 0;
         }
-        else {
+        else
+        {
             this->gVal = parent->gVal + 1;
         }
 
-        if(hFlag == 0) {
+        if (hFlag == 0)
+        {
             this->hVal = this->getHammingDistance();
         }
-        else if(hFlag == 1) {
+        else if (hFlag == 1)
+        {
             this->hVal = this->getManhattanDistance();
         }
-        
+
         this->fVal = this->gVal + this->hVal;
     }
 
-    int getGVal() {
+    int getGVal()
+    {
         return this->gVal;
     }
 
-    int getHammingDistance() {
+    int getFVal() {
+        return this->fVal;
+    }
+
+    int getHammingDistance()
+    {
         int hammingDistance = 0;
-        for(int i = 0; i < this->gridSize; i++) {
-            for(int j = 0; j < this->gridSize; j++) {
-                if(this->board[i][j] != 0) {
-                    if(this->board[i][j] != (this->gridSize * i + j) + 1) {
+        for (int i = 0; i < this->gridSize; i++)
+        {
+            for (int j = 0; j < this->gridSize; j++)
+            {
+                if (this->board[i][j] != 0)
+                {
+                    if (this->board[i][j] != (this->gridSize * i + j) + 1)
+                    {
                         hammingDistance++;
                     }
                 }
@@ -55,11 +69,15 @@ public:
         return hammingDistance;
     }
 
-    int getManhattanDistance() {
+    int getManhattanDistance()
+    {
         int manhattanDistance = 0;
-        for(int i = 0; i < this->gridSize; i++) {
-            for(int j = 0; j < this->gridSize; j++) {
-                if(this->board[i][j] != 0) {
+        for (int i = 0; i < this->gridSize; i++)
+        {
+            for (int j = 0; j < this->gridSize; j++)
+            {
+                if (this->board[i][j] != 0)
+                {
                     int goal_i = (this->board[i][j] - 1) / this->gridSize;
                     int goal_j = (this->board[i][j] - 1) % this->gridSize;
                     manhattanDistance += (abs(i - goal_i) + abs(j - goal_j));
@@ -69,10 +87,14 @@ public:
         return manhattanDistance;
     }
 
-    int getBlankTilePos() {
-        for(int i = 0; i < this->gridSize; i++) {
-            for(int j = 0; j < this->gridSize; j++) {
-                if(this->board[i][j] == 0) {
+    int getBlankTilePos()
+    {
+        for (int i = 0; i < this->gridSize; i++)
+        {
+            for (int j = 0; j < this->gridSize; j++)
+            {
+                if (this->board[i][j] == 0)
+                {
                     return (this->gridSize * i + j);
                 }
             }
@@ -80,12 +102,16 @@ public:
         return -1;
     }
 
-    int getInversionCount() {
+    int getInversionCount()
+    {
         set<int> s;
         int invCnt = 0;
-        for(int i = this->gridSize - 1; i >= 0; i--) {
-            for(int j = this->gridSize - 1; j >= 0; j--) {
-                if(this->board[i][j] != 0) {
+        for (int i = this->gridSize - 1; i >= 0; i--)
+        {
+            for (int j = this->gridSize - 1; j >= 0; j--)
+            {
+                if (this->board[i][j] != 0)
+                {
                     s.insert(this->board[i][j]);
                     auto it = s.lower_bound(this->board[i][j]);
                     invCnt += distance(s.begin(), it);
@@ -95,57 +121,76 @@ public:
         return invCnt;
     }
 
-    bool canUp() {
-        if(this->parMove == 2) {
+    bool canUp()
+    {
+        if (this->parMove == 2)
+        {
             return false;
         }
-        else if((this->getBlankTilePos() / this->gridSize) <= 0) {
+        else if ((this->getBlankTilePos() / this->gridSize) <= 0)
+        {
             return false;
         }
-        else {
+        else
+        {
             return true;
         }
     }
 
-    bool canDown() {
-        if(this->parMove == 1) {
+    bool canDown()
+    {
+        if (this->parMove == 1)
+        {
             return false;
         }
-        else if((this->getBlankTilePos() / this->gridSize) >= this->gridSize - 1) {
+        else if ((this->getBlankTilePos() / this->gridSize) >= this->gridSize - 1)
+        {
             return false;
         }
-        else {
+        else
+        {
             return true;
         }
     }
 
-    bool canRight() {
-        if(this->parMove == 4) {
+    bool canRight()
+    {
+        if (this->parMove == 4)
+        {
             return false;
         }
-        else if((this->getBlankTilePos() % this->gridSize) >= this->gridSize - 1) {
+        else if ((this->getBlankTilePos() % this->gridSize) >= this->gridSize - 1)
+        {
             return false;
         }
-        else {
+        else
+        {
             return true;
         }
     }
 
-    bool canLeft() {
-        if(this->parMove == 3) {
+    bool canLeft()
+    {
+        if (this->parMove == 3)
+        {
             return false;
         }
-        else if((this->getBlankTilePos() % this->gridSize) <= 0) {
+        else if ((this->getBlankTilePos() % this->gridSize) <= 0)
+        {
             return false;
         }
-        else {
+        else
+        {
             return true;
         }
     }
 
-    void printBoard() {
-        for(int i = 0; i < this->gridSize; i++) {
-            for(int j = 0; j < this->gridSize; j++) {
+    void printBoard()
+    {
+        for (int i = 0; i < this->gridSize; i++)
+        {
+            for (int j = 0; j < this->gridSize; j++)
+            {
                 cout << this->board[i][j] << ' ';
             }
             cout << '\n';
@@ -153,7 +198,8 @@ public:
         cout << '\n';
     }
 
-    Board* getUpNeighbour() {
+    Board *getUpNeighbour()
+    {
         vector<vector<int>> temp = this->board;
         int blank_i = this->getBlankTilePos() / this->gridSize;
         int blank_j = this->getBlankTilePos() % this->gridSize;
@@ -161,7 +207,8 @@ public:
         return (new Board(this->gridSize, temp, this, this->hFlag, 1));
     }
 
-    Board* getDownNeighbour() {
+    Board *getDownNeighbour()
+    {
         vector<vector<int>> temp = this->board;
         int blank_i = this->getBlankTilePos() / this->gridSize;
         int blank_j = this->getBlankTilePos() % this->gridSize;
@@ -169,7 +216,8 @@ public:
         return (new Board(this->gridSize, temp, this, this->hFlag, 2));
     }
 
-    Board* getRightNeighbour() {
+    Board *getRightNeighbour()
+    {
         vector<vector<int>> temp = this->board;
         int blank_i = this->getBlankTilePos() / this->gridSize;
         int blank_j = this->getBlankTilePos() % this->gridSize;
@@ -177,31 +225,86 @@ public:
         return (new Board(this->gridSize, temp, this, this->hFlag, 3));
     }
 
-    Board* getLeftNeighbour() {
+    Board *getLeftNeighbour()
+    {
         vector<vector<int>> temp = this->board;
         int blank_i = this->getBlankTilePos() / this->gridSize;
         int blank_j = this->getBlankTilePos() % this->gridSize;
         swap(temp[blank_i][blank_j - 1], temp[blank_i][blank_j]);
         return (new Board(this->gridSize, temp, this, this->hFlag, 4));
     }
-
 };
 
-class AStar {
+class Compare
+{
+public:
+    bool operator() (Board *a, Board *b) {
+        return a->getFVal() < b->getFVal();
+    }
+};
+
+class AStar
+{
 
 private:
-    Board* initialBoard;
+    Board *initialBoard;
     vector<vector<int>> goalBoard;
     int gridSize;
-    set<Board*> boards;
-    // need another set / priority queue
+    set<Board *> boards;
+    priority_queue<Board*, vector<Board*>, Compare> pq;
+
+    void makeGoalBoard()
+    {
+        this->goalBoard.resize(this->gridSize, vector<int>(this->gridSize));
+        for (int i = 0; i < this->gridSize; i++)
+        {
+            for (int j = 0; j < this->gridSize; j++)
+            {
+                this->goalBoard[i][j] = this->gridSize * i + j + 1;
+            }
+        }
+        this->goalBoard[this->gridSize - 1][this->gridSize - 1] = 0;
+    }
 
 public:
+    AStar(int gridSize, vector<vector<int>> initialBoard, int hFlag)
+    {
+        this->gridSize = gridSize;
+        this->initialBoard = new Board(gridSize, initialBoard, nullptr, hFlag, -1);
+        this->boards.insert(this->initialBoard);
+        this->makeGoalBoard();
+        this->pq.push(this->initialBoard);
+    }
 
+    bool isSolvable()
+    {
+        int invCnt = this->initialBoard->getInversionCount();
+        if (this->gridSize & 1)
+        {
+            return !(invCnt & 1);
+        }
+        else
+        {
+            int blank_row = this->initialBoard->getBlankTilePos() / this->gridSize;
+            return (blank_row & 1) ^ (invCnt & 1);
+        }
+    }
 
+    void search()
+    {
+    }
+
+    void freeUpMemory()
+    {
+        for (Board *x : this->boards)
+        {
+            delete x;
+        }
+    }
 };
 
-int main() {
+int main()
+{
 
     freopen("in.txt", "r", stdin);
 
@@ -214,15 +317,19 @@ int main() {
 
     vector<vector<int>> v(n);
 
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         getline(cin, s);
         istringstream ss(s);
-        while(ss >> s) {
+        while (ss >> s)
+        {
             v[i].push_back(stoi(s));
         }
     }
 
-    Board *initialBoard = new Board(n, v, nullptr, 0, -1);
-    
+    AStar graph(n, v, 0);
+
+    graph.freeUpMemory();
+
     return 0;
 }

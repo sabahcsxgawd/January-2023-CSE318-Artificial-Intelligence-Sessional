f2d7bc58e952016bba0df1af312b4a088af3c954
sabahcsxgawd <sabah72224@gmail.com>
2023-07-17 23:05:11 +0600

    need to fix mem leak

--- Offline 2/mancalacpp/git_push.sh
+++ Offline 2/mancalacpp/git_push.sh
@@ -2,5 +2,5 @@
 
 cd "/media/sabah/269CFD019CFCCC75/Level 3 Term 2/Sessional/January 2023 CSE318 Artificial Intelligence Sessional"
 git add --all
-git commit -m "ansan"
+git commit -m "$1"
 git push -u origin master

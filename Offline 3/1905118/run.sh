cd24523968b7596e636febd5b97343cec62d8f1f
sabahcsxgawd <sabah72224@gmail.com>
2023-08-21 12:50:36 +0600

    gee

--- Offline 3/1905118/run.sh
+++ Offline 3/1905118/run.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
-g++ -std=c++17 -fsanitize=address 1905118.cpp -o 1905118
+g++ -std=c++17 -fsanitize=address 1905118gg.cpp -o 1905118gg
 # g++ -std=c++17 -g 1905118.cpp -o 1905118
-
-gnome-terminal -- bash -c "./run1.sh; exec bash"
-gnome-terminal -- bash -c "./run2.sh; exec bash"
-gnome-terminal -- bash -c "./run3.sh; exec bash"
-gnome-terminal -- bash -c "./run4.sh; exec bash"
+for i in $(seq 1 1 54)
+do
+    echo $i $(./1905118gg < ../set1/g$i.rud)
+done
+# gnome-terminal -- bash -c "./run1.sh; exec bash"
+# gnome-terminal -- bash -c "./run2.sh; exec bash"
+# gnome-terminal -- bash -c "./run3.sh; exec bash"
+# gnome-terminal -- bash -c "./run4.sh; exec bash"

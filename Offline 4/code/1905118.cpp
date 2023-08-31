bdd6e924b162e03c3dea1c5c6b71454699f9fbd8
sabahcsxgawd <sabah72224@gmail.com>
2023-09-01 03:11:59 +0600

    gege

--- Offline 4/code/1905118.cpp
+++ Offline 4/code/1905118.cpp
@@ -7,28 +7,33 @@ class Node
 private:
     int attributeIndex;
     string exampleBitMap;
-    vector<Node*> children;
+    vector<Node *> children;
 
 public:
     Node(const Node &) = delete;
 
-    Node(string exampleBitMap) {
+    Node(string exampleBitMap)
+    {
         this->exampleBitMap = exampleBitMap;
     }
 
-    void setAttributeIndex(int attributeIndex) {
+    void setAttributeIndex(int attributeIndex)
+    {
         this->attributeIndex = attributeIndex;
     }
 
-    void addChildren(Node *node) {
+    void addChildren(Node *node)
+    {
         this->children.emplace_back(node);
     }
 
-    int getAttrIndex() {
+    int getAttributeIndex()
+    {
         return this->attributeIndex;
     }
 
-    vector<Node*> getChildren() {
+    vector<Node *> getChildren()
+    {
         return this->children;
     }
 };
@@ -41,7 +46,7 @@ private:
     vector<vector<int>> trainingData, testData; // training is actually examples
     vector<int> howManyValsPerAttr;
     map<int, string> goalClassValues;
-    
+
     Node *root;
 
     bool isEmpty(const string &s)
@@ -81,18 +86,23 @@ private:
         return entropy;
     }
 
-    Node* getPluralityValue(const string &s) {
+    Node *getPluralityValue(const string &exampleBitMap)
+    {
         vector<int> goalClassValues(this->howManyValsPerAttr[this->goalIndex], 0);
         int i = 0;
-        for(char c : s) {
-            if(c == '1') {
+        for (char c : exampleBitMap)
+        {
+            if (c == '1')
+            {
                 goalClassValues[this->trainingData[i][goalIndex]]++;
             }
             i++;
         }
         int maxGoalClassIndex = 0;
-        for(i = 1; i < goalClassValues.size(); i++) {
-            if(goalClassValues[i] > goalClassValues[maxGoalClassIndex]) {
+        for (i = 1; i < goalClassValues.size(); i++)
+        {
+            if (goalClassValues[i] > goalClassValues[maxGoalClassIndex])
+            {
                 maxGoalClassIndex = i;
             }
         }
@@ -101,15 +111,47 @@ private:
         return rv;
     }
 
-    void freeMemory(Node *root) {
-        if(root->getAttrIndex() == this->goalIndex) {
-            delete root;
-        }
-        else {
-            for(Node *child : root->getChildren()) {
-                freeMemory(child);
+    int isSameClassification(const string &exampleBitMap)
+    {
+        int fixedGoalClassIndex = -1;
+        int i = 0;
+        for (char c : exampleBitMap)
+        {
+            if (c == '1')
+            {
+                if (fixedGoalClassIndex == -1)
+                {
+                    fixedGoalClassIndex = this->trainingData[i][this->goalIndex];
+                }
+                else
+                {
+                    if (fixedGoalClassIndex != this->trainingData[i][this->goalIndex])
+                    {
+                        return -1;
+                    }
+                }
             }
-            delete root;
+            i++;
+        }
+        return fixedGoalClassIndex;
+    }
+
+    void freeMemory(Node *root)
+    {
+        if (root != NULL)
+        {
+            if (root->getAttributeIndex() == this->goalIndex)
+            {
+                delete root;
+            }
+            else
+            {
+                for (Node *child : root->getChildren())
+                {
+                    freeMemory(child);
+                }
+                delete root;
+            }
         }
     }
 
@@ -119,6 +161,7 @@ public:
     DecisionTree()
     {
         this->howManyAttrs = this->howManyExamples = this->goalIndex = 0;
+        this->root = NULL;
     }
 
     void feedData(const string &data)
@@ -164,7 +207,8 @@ public:
             for (int j = 0; j < tempData[i].size(); j++)
             {
                 replaceAttrValues.emplace_back(attrValMapper[{j, tempData[i][j]}]);
-                if(j == this->goalIndex && this->goalClassValues.find(replaceAttrValues.back()) == this->goalClassValues.end()) {
+                if (j == this->goalIndex && this->goalClassValues.find(replaceAttrValues.back()) == this->goalClassValues.end())
+                {
                     this->goalClassValues[replaceAttrValues.back()] = tempData[i][j];
                 }
             }
@@ -191,33 +235,41 @@ public:
         inFile.close();
     }
 
-    Node* learnByInfoGain(string exampleBitMap, string attributeBitMap, string parentExampleBitMap) {
-        if(this->isEmpty(exampleBitMap)) {
+    Node *learnByInfoGain(string exampleBitMap, string attributeBitMap, string parentExampleBitMap)
+    {
+        int sameGoalClass = -1;
+        if (this->isEmpty(exampleBitMap))
+        {
             return this->getPluralityValue(parentExampleBitMap);
         }
-        // else if() {
-
-        // }
-        else if(this->isEmpty(attributeBitMap)) {
+        else if ((sameGoalClass = this->isSameClassification(exampleBitMap)) != -1)
+        {
+            Node *rv = new Node(this->goalClassValues[sameGoalClass]);
+            rv->setAttributeIndex(this->goalIndex);
+            return rv;
+        }
+        else if (this->isEmpty(attributeBitMap))
+        {
             return this->getPluralityValue(exampleBitMap);
         }
-        else {
-
+        else
+        {
         }
         return NULL;
     }
 
-    void learn() {
+    void learn()
+    {
         string exampleBitMap(this->howManyExamples, '1');
         string attributeBitMap(this->howManyAttrs, '1');
         string parentExampleBitMap(this->howManyExamples, '1');
         this->root = learnByInfoGain(exampleBitMap, attributeBitMap, parentExampleBitMap);
     }
 
-    void free() {
+    void free()
+    {
         this->freeMemory(this->root);
     }
-
 };
 
 int main(void)
@@ -225,5 +277,6 @@ int main(void)
     DecisionTree d;
     d.feedData("car.data");
     d.learn();
+    d.free();
     return 0;
 }

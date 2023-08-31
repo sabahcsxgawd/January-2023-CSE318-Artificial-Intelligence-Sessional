a1b42ffbd1cfad6d22f6a0ec20bd8fb2472c4d5e
sabahcsxgawd <sabah72224@gmail.com>
2023-08-31 22:16:23 +0600

    baal

--- Offline 4/code/1905118.cpp
+++ Offline 4/code/1905118.cpp
@@ -1,44 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class DecisionTree
+{
 
-bool isEmpty(const string &s) {
-    for(char c : s) {
-        if(c == '1') {
-            return false;
+private:
+    int howManyAttrs, howManyExamples, goalIndex;
+    vector<vector<int>> howManyValsPerAttr, trainingData, testData; // training is actually examples
+
+    bool isEmpty(const string &s)
+    {
+        for (char c : s)
+        {
+            if (c == '1')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    double myLog(double p)
+    {
+        if (p > 0.0)
+        {
+            return log2(p);
+        }
+        else
+        {
+            return 0.0;
         }
     }
-    return true;
-}
 
-double myLog(double p) {
-    if(p > 0.0) {
-        return log2(p);
+    double getEntropy(const vector<int> &v)
+    {
+        double total, multPart, entropy;
+        total = multPart = 0.0;
+        for (int x : v)
+        {
+            total += (double)x;
+            multPart += ((double)x * myLog(x));
+        }
+        entropy = myLog(total) - (multPart / total);
+        return entropy;
     }
-    else {
-        return 0.0;
-    }
-}
 
-double getEntropy(const vector<int> &v) {
-    double total, multPart, entropy;
-    total = multPart = 0.0;
-    for(int x : v) {
-        total += (double)x;
-        multPart += ((double)x * myLog(x));
-    }
-    entropy = myLog(total) - (multPart / total);
-    return entropy;
-}
+public:
+    DecisionTree(const DecisionTree &) = delete;
 
-int main(void) {
+    DecisionTree()
+    {
+        this->howManyAttrs = this->howManyExamples = this->goalIndex = 0;
+    }
+
+    DecisionTree(const string &data)
+    {
+        // asssuming test data is in same folder with correct file name
+        ifstream inFile(data);
+        string line, token;
+        stringstream ss;
+        vector<string> tempAttrValues;
+        map<pair<int, string>, int> attrValMapper;
+        while (getline(inFile, line))
+        {
+            ss.str(line);
+            this->howManyExamples++;
+            tempAttrValues.clear();
+            while (getline(ss, token, ','))
+            {
+                tempAttrValues.push_back(token);
+                if (this->howManyExamples == 1)
+                {
+                    this->howManyAttrs++;
+                    // TODO
+                }
+            }
+        }
+    }
+};
+
+int main(void)
+{
     ifstream inFile("car.data");
     string line, token;
     stringstream ss;
-    while(getline(inFile, line)) {
-        cout << line << '\n';        
+    while (getline(inFile, line))
+    {
+        cout << line << '\n';
         ss.str(line);
-        while(getline(ss, token, ',')) {
+        while (getline(ss, token, ','))
+        {
             cout << token << '|';
         }
         break;

fa3fb41c5b6fbd9a98daa6e437afcb9a401177e9
sabahcsxgawd <sabah72224@gmail.com>
2023-09-01 15:59:54 +0600

    gege

--- Offline 4/code/decision_tree.py
+++ Offline 4/code/decision_tree.py
@@ -20,7 +20,7 @@ y = pima['outcome']# Target variable
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=1) # 70% training and 30% test
 
 # Create Decision Tree classifer object
-clf = DecisionTreeClassifier(criterion="entropy")
+clf = DecisionTreeClassifier(criterion="entropy", max_depth=6)
 
 # Train Decision Tree Classifer
 clf = clf.fit(X_train,y_train)
@@ -31,9 +31,9 @@ y_pred = clf.predict(X_test)
 
 # Model Accuracy, how often is the classifier correct?
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred) * 100.0)
-# fig = plt.figure(figsize=(25,20))
-# tree.plot_tree(clf)
-# fig.savefig("decistion_tree.png")
+fig = plt.figure(figsize=(30,18))
+tree.plot_tree(clf)
+fig.savefig("decistion_tree.png")
 
 
 
